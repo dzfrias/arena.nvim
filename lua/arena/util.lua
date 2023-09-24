04bd6406aa8f71dd4334869c60b21f26b6a38a0b
Diego <96022404+dzfrias@users.noreply.github.com>
2023-09-24 08:39:40 -0700

    fix: truncate paths correctly

--- lua/arena/util.lua
+++ lua/arena/util.lua
@@ -1,40 +1,51 @@
 local M = {}
 
+local function ancestors(path)
+  local seen = {}
+  local components = vim.fn.split(path, "/")
+  for i = 1, #components do
+    local slice = { unpack(components, i) }
+    table.insert(seen, vim.fn.join(slice, "/"))
+  end
+
+  return seen
+end
+
 --- Truncate paths intelligently. Given { "~/test/mod.rs", "~/test/more/mod.rs" },
 --- this function will truncate the paths (in-place) to
 --- { "test/mod.rs", "more/mod.rs" }.
 --- @param paths string[]
---- @param opts { always_context: string[] }
+--- @param opts { always_context: string[] }?
 function M.truncate_paths(paths, opts)
   opts = opts or {
     always_context = {},
   }
 
-  local components = {}
+  local seen = {}
   for _, path in ipairs(paths) do
-    local basename = vim.fs.basename(path)
-    components[basename] = (components[basename] or 0) + 1
+    for _, anc in ipairs(ancestors(path)) do
+      seen[anc] = (seen[anc] or 0) + 1
+    end
   end
 
-  for _, basename in ipairs(opts.always_context) do
-    components[basename] = (components[basename] or 0) + 1
+  for _, p in ipairs(opts.always_context) do
+    seen[p] = (seen[p] or 0) + 1
   end
 
   for i, path in ipairs(paths) do
-    local needed = components[vim.fs.basename(path)]
-    local comps = vim.fn.split(path, "/")
-    local final_path = ""
-    while needed ~= 0 do
-      local sep
-      if final_path == "" then
-        sep = ""
-      else
-        sep = "/"
+    local found = false
+    local ancstrs = ancestors(path)
+    for j = #ancstrs, 1, -1 do
+      local anc = ancstrs[j]
+      paths[i] = anc
+      if seen[anc] == 1 then
+        found = true
+        break
       end
-      final_path = final_path .. sep .. comps[#comps - needed + 1]
-      needed = needed - 1
     end
-    paths[i] = final_path
+    if not found then
+      paths[i] = ancstrs[-1]
+    end
   end
 end
 
